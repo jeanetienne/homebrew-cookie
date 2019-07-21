75d5d362d75b4a51b9c80dafec738cda8099d72e
Jean-Étienne <git@jeanetienne.net>
2019-07-21 16:13:54 +1000

    Updated formula for version 0.3.0

--- Formula/cookie.rb
+++ Formula/cookie.rb
@@ -1,8 +1,11 @@
 class Cookie < Formula
   desc "Command-line cookie cutter for your app icons and Xcode assets"
   homepage "https://github.com/jeanetienne/Cookie"
-  url "https://github.com/jeanetienne/Cookie/archive/0.2.0.tar.gz"
-  sha256 "07428b767c3ed21bb74cf218bd1181ea5befe1c216b44945b1ae1f0b734d5840"
+  head "https://github.com/jeanetienne/Cookie.git"
+
+  version "0.3.0"
+  url "https://github.com/jeanetienne/Cookie/archive/#{version}.tar.gz"
+  sha256 "2f9d889761ee151118a51f2d76eab3546e1d1778fec2117f5a60afce29bc62c0"
 
   depends_on :xcode => ["10.0", :build]
 
